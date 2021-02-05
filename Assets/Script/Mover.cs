5f99b87720e9dfd7fe32973f43f3effa2a8ffd54
as <>
2021-02-06 02:49:13 +0900

    NavMeshAgentを使う、スクリプト

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform taget;
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<NavMeshAgent>().destination = taget.position;
     }
 }
