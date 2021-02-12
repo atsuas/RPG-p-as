0d27dd75aa310a9805070741a40d57fa5d710c8d
as <>
2021-02-12 22:44:36 +0900

    エラーを消去、スクリプト

--- Assets/Script/Control/PlayerController.cs
+++ Assets/Script/Control/PlayerController.cs
@@ -40,7 +40,7 @@ namespace RPG.control
             {
                 if (Input.GetMouseButtonDown(0))
                 {
-                    GetComponent<Mover>().MoveTo(hit.point);
+                    GetComponent<Mover>().StartMoveAction(hit.point);
                 }
                 return true;
             }
