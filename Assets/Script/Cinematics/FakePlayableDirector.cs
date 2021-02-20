44e844a34f0650f45b4bdb551d9a07c19d27e8d2
as <>
2021-02-21 06:11:52 +0900

    オブザーバーパターン

---
+++ Assets/Script/Cinematics/FakePlayableDirector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace RPG.Cinematics
+{
+    public class FakePlayableDirector : MonoBehaviour
+    {
+        public event Action<float> onFinish;
+
+        private void Start()
+        {
+            Invoke("OnFinish", 3f);
+        }
+
+        void OnFinish()
+        {
+            onFinish(4.3f);
+        }
+    }
+}
+
+
