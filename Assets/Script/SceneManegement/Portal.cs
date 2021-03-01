1f51e5b6843861be138604f5e1289c75d390ef18
as <>
2021-03-02 07:38:15 +0900

    複数のSpawn Pointを作成

--- Assets/Script/SceneManegement/Portal.cs
+++ Assets/Script/SceneManegement/Portal.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.AI; // 追加
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        //追加
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int sceneToLoad = -1;
-        [SerializeField] Transform spawnPoint;  //追加
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination; //追加
 
         private void OnTriggerEnter(Collider other)
         {
@@ -22,33 +29,39 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            //追加
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
-            Portal otherPortal = GetOtherPortal();    //追加
-            UpdatePlayer(otherPortal);  //追加
-
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
             Destroy(gameObject);
         }
 
-        //追加
+
         private void UpdatePlayer(Portal otherPortal)
         {
             GameObject player = GameObject.FindWithTag("Player");
             player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
             player.transform.rotation = otherPortal.spawnPoint.rotation;
+            //player.transform.position = otherPortal.spawnPoint.position;
         }
 
-        //追加
         private Portal GetOtherPortal()
         {
             foreach (Portal portal in FindObjectsOfType<Portal>())
             {
                 if (portal == this) continue;
+                if (portal.destination != destination) continue;
 
                 return portal;
             }
-
             return null;
         }
     }
