bba145d949dff76ca34da09fdac65dbb149eff59
as <>
2021-03-04 04:15:17 +0900

    writeByteについて

--- Assets/Script/Saving/SavingSystem.cs
+++ Assets/Script/Saving/SavingSystem.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace RPG.Saving
@@ -8,12 +10,29 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            print("Saving to " + saveFile);
+            string path = GetPathFromSaveFile(saveFile);
+            print("Saving to " + path); //追加
+            FileStream stream = File.Open(path, FileMode.Create);　//追加
+            stream.WriteByte(0xc2); //追加
+            stream.WriteByte(0xa1); //追加
+            stream.WriteByte(0x48); //追加
+            stream.WriteByte(0x6f); //追加
+            stream.WriteByte(0x6c); //追加
+            stream.WriteByte(0x61); //追加
+            stream.WriteByte(0x21); //追加
+            byte[] bytes = Encoding.UTF8.GetBytes("!Hola Mundo!"); //追加
+            stream.Write(bytes, 0, bytes.Length); //追加
+            stream.Close(); //追加
         }
 
         public void Load(string saveFile)
         {
-            print("Load to " + saveFile);
+            print("Load to " + GetPathFromSaveFile(saveFile));
+        }
+
+        private string GetPathFromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
         }
     }
 }
