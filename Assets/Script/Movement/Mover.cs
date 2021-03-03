0d4a77f6799399cec2047029e87b801212f2a639
as <>
2021-03-03 23:26:40 +0900

    BZ セーブ位置を記憶する２

--- Assets/Script/Movement/Mover.cs
+++ Assets/Script/Movement/Mover.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
-using RPG.Saving; //追加
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction, ISaveable //ISaveableを追加
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField] Transform target;
         [SerializeField] float maxSpeed = 6f;
@@ -55,17 +55,20 @@ namespace RPG.Movement
             GetComponent<Animator>().SetFloat("fowardSpeed", speed);
         }
 
-        //追加
         public object CaptureState()
         {
-            return new SerializableVector3(transform.position);
+            Dictionary<string, object> data = new Dictionary<string, object>(); //追加
+            data["position"] = new SerializableVector3(transform.position); //追加
+            data["rotation"] = new SerializableVector3(transform.eulerAngles); //追加
+            return data; //追加
         }
-        //追加
+
         public void RestoreState(object state)
         {
-            SerializableVector3 position = (SerializableVector3)state;
+            Dictionary<string, object> data = (Dictionary<string, object>)state; //変更
             GetComponent<NavMeshAgent>().enabled = false;
-            transform.position = position.ToVector();
+            transform.position = ((SerializableVector3)data["position"]).ToVector(); //変更
+            transform.eulerAngles = ((SerializableVector3)data["rotation"]).ToVector(); //追加
             GetComponent<NavMeshAgent>().enabled = true;
         }
     }
