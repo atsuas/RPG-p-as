89472b9dac49f972040567d980ac6ea6f3638d28
as <>
2021-03-04 04:16:59 +0900

    Byteについて

--- Assets/Script/Saving/SavingSystem.cs
+++ Assets/Script/Saving/SavingSystem.cs
@@ -13,13 +13,6 @@ namespace RPG.Saving
             string path = GetPathFromSaveFile(saveFile);
             print("Saving to " + path); //追加
             FileStream stream = File.Open(path, FileMode.Create);　//追加
-            stream.WriteByte(0xc2); //追加
-            stream.WriteByte(0xa1); //追加
-            stream.WriteByte(0x48); //追加
-            stream.WriteByte(0x6f); //追加
-            stream.WriteByte(0x6c); //追加
-            stream.WriteByte(0x61); //追加
-            stream.WriteByte(0x21); //追加
             byte[] bytes = Encoding.UTF8.GetBytes("!Hola Mundo!"); //追加
             stream.Write(bytes, 0, bytes.Length); //追加
             stream.Close(); //追加
