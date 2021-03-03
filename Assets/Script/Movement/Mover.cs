aa8a2f9513f550ec6d24d7ec046f60c219353d71
as <>
2021-03-04 03:10:47 +0900

    Saving System.cs, SavingWrapper.csの作成

--- Assets/Script/Movement/Mover.cs
+++ Assets/Script/Movement/Mover.cs
@@ -7,7 +7,7 @@ using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction, ISaveable
+    public class Mover : MonoBehaviour, IAction
     {
         [SerializeField] Transform target;
         [SerializeField] float maxSpeed = 6f;
@@ -55,17 +55,6 @@ namespace RPG.Movement
             GetComponent<Animator>().SetFloat("fowardSpeed", speed);
         }
 
-        public object CaptureState()
-        {
-            return new SerializableVector3(transform.position);
-        }
-
-        public void RestoreState(object state)
-        {
-            SerializableVector3 position = (SerializableVector3)state;
-            GetComponent<NavMeshAgent>().enabled = false;
-            transform.position = position.ToVector();
-            GetComponent<NavMeshAgent>().enabled = true;
-        }
+        
     }
 }
--- Assets/Script/SceneManegement/Portal.cs
+++ Assets/Script/SceneManegement/Portal.cs
@@ -46,17 +46,16 @@ namespace RPG.SceneManagement
 
             yield return fader.FadeOut(fadeOutTime);
 
-            SavingWrapper wrapper = FindObjectOfType<SavingWrapper>();
-            wrapper.Save();
+            
 
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
-            wrapper.Load();
+           
 
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
 
-            wrapper.Save(); //追加
+           
 
             yield return new WaitForSeconds(fadeWaitTime);
             yield return fader.FadeIn(fadeInTime);
