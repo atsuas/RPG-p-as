c4f3d441f90cf072e791e43b96d72df91518bac4
as <>
2021-03-05 10:11:14 +0900

    Healthポイントの保存

--- Assets/Script/Movement/Mover.cs
+++ Assets/Script/Movement/Mover.cs
@@ -7,7 +7,7 @@ using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField] Transform target;
         [SerializeField] float maxSpeed = 6f;
@@ -55,6 +55,18 @@ namespace RPG.Movement
             GetComponent<Animator>().SetFloat("fowardSpeed", speed);
         }
 
-        
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
     }
 }
